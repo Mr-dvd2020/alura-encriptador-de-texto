f439c0deaf208db3b9f03fdbb08bc2203193eef2
Divcode <davidsonco074@gmail.com>
2024-07-09 23:05:32 -0500

    configuración en modo responsive

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ function encriptar(){
     let tutuloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
     let muneco = document.getElementById("dibujo");
+    let mensaje = document.getElementById("mensaje")
     if(texto.length !=0){
         document.getElementById("texto").value=textoCifrado;
         document.getElementById("mensaje").value=textoCifrado
         tutuloMensaje.textContent = "Texto encriptado con éxito";
         parrafo.textContent= "";
+        mensaje.style.display = "block";
         muneco.style.display = "none";
     }
     else{
+        mensaje.style.display = "none";
         muneco.style.display = "block";
         tutuloMensaje.textContent = "Ningún Mensaje Encontrado";
         parrafo.textContent = "Ingresa el mensaje que deseas Encriptar o Desencriptar"; 
@@ -24,4 +27,34 @@ function encriptar(){
     }
 }
 
+function desencriptar(){
+    let texto =document.getElementById("texto").value;
+    let tutuloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("dibujo");
+    let mensaje = document.getElementById("mensaje")
+
+    let textoCifrado = texto
+    .replace(/enter/gi,"e")
+    .replace(/ines/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "a");
+
+    if(texto.length !=0){
+        document.getElementById("texto").value=textoCifrado;
+        tutuloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent= "";
+        muneco.style.display = "none";
+        mensaje.style.display = "block";
+    }
+    else{
+
+        muneco.style.display = "block";
+        mensaje.style.display = "none";
+        tutuloMensaje.textContent = "Ningún Mensaje Encontrado";
+        parrafo.textContent = "Ingresa el mensaje que deseas Encriptar o Desencriptar"; 
+        alert("Debes Igresar algún texto");
+    }
+}
 
